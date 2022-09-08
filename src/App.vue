e193bbad8178caf95375f561ee46debb7aafc468
Lazyzk <f1651998087@163.com>
2022-09-08 22:56:06 +0800

    测试

--- src/App.vue
+++ src/App.vue
@@ -14,6 +14,7 @@ import Footer from './components/Footer';
 export default {
   name: "App",
   mounted() {
+    //派遣数据
     this.$store.dispatch('getCategoryList')
   },
   components: { Header, Footer }
