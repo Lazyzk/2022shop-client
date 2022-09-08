4577f78a7ef56dd4442da3417a65ab0ca3d80ee6
Lazyzk <f1651998087@163.com>
2022-09-08 17:05:51 +0800

    第四次

--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import TypeNav from '@/components/TypeNav'
 Vue.config.productionTip = false
-
+Vue.component(TypeNav.name, TypeNav)
 new Vue({
   render: h => h(App),
   router,//注册路由器==>所有组件都可以直接访问2个对象：$router(路由器对象)与$route（当前路由信息对象）
--- src/pages/Home/index.vue
+++ src/pages/Home/index.vue
@@ -1,14 +1,28 @@
 <template>
   <div>
-    Home
+    <TypeNav></TypeNav>
+    <ListContainer></ListContainer>
+    <TodayRecommend></TodayRecommend>
+    <Rank></Rank>
+    <Like></Like>
+    <Floor></Floor>
+    <Floor></Floor>
+    <Brand></Brand>
   </div>
 </template>
 
 <script>
+import TypeNav from '@/components/TypeNav/index.vue';
+import ListContainer from './ListContainer/index.vue';
+import TodayRecommend from './TodayRecommend/index.vue';
+import Rank from './Rank/index.vue';
+import Like from './Like/index.vue';
+import Floor from './Floor/index.vue';
+import Brand from './Brand/index.vue';
 
 export default {
   name: 'Home',
-  components: {},
+  components: { TypeNav, ListContainer, TodayRecommend, Rank, Like, Floor, Brand },
   props: {},
 
   data() {
